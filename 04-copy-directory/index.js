67ff8198cf39975e91c82d26e2c9650c2f60e2f7
VasilinaFreedom <vasilinamkrvch@gmail.com>
2022-05-22 20:10:24 +0300

    feat: add solution copy-directory (05/22/2022 20:10:24)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+const fs = require("fs");
+const path = require("path");
 
+
+function copyDir() {
+    fs.mkdir(path.resolve(__dirname, 'files-copy'), { recursive: true }, (err) => {
+        if (err) throw err;
+      });
+
+//копируем файлы
+    fs.readdir(path.join(__dirname, 'files'), { withFileTypes: true },
+        (err, files) => {
+          console.log('Список файлов:');
+          if (err) console.log(err);
+          else {
+            files.forEach((file) => {
+                const folder = path.join(__dirname, 'files', file.name); //обращаемся к папкам
+                const folderCopy = path.join(__dirname, 'files-copy', file.name);
+              fs.copyFile(folder, folderCopy, () => {
+                console.log(`${file.name} скопирован`);
+              });
+            });
+}})}
+
+copyDir()
